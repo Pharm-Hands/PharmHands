48fbc660b970b78979656d58df3175a345971f25
jaya <jayaseyyadri@gmail.com>
2021-03-08 16:29:27 -0600

    setting up project

---
+++ src/main/java/com/pharmhands/PharmHandsApplication.java
@@ -0,0 +1,13 @@
+package com.pharmhands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmHandsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmHandsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pharmhands/PharmHandsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharmhands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmHandsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
