ab076394b4ea17e15a40dfb30da791e330fb709f
jaya <jayaseyyadri@gmail.com>
2021-03-11 09:21:28 -0600

    added repositories and UserRoles

---
+++ src/main/java/com/pharmhands/SecurityConfiguration.java
@@ -0,0 +1,4 @@
+package com.pharmhands;
+
+public class SecurityConfiguration {
+}
---
+++ src/main/java/com/pharmhands/models/Prescriptions.java
@@ -0,0 +1,12 @@
+//package com.pharmhands.models;
+//
+//import org.springframework.boot.autoconfigure.domain.EntityScan;
+//
+//@Entity
+//@Table(name="prescriptions")
+//public class Prescriptions {
+//@GeneratedValue(strategy =GenerationType.IDENTITY)
+//private long id;
+//
+
+//}
