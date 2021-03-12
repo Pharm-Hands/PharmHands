574aaa4af61726d9870319d52a934d7bae86f922
Joe Cuthbert <josiah.thomas.cuthbert@gmail.com>
2021-03-12 10:38:13 -0600

    basic security added

--- src/main/java/com/pharmhands/repositories/UserRepository.java
+++ src/main/java/com/pharmhands/repositories/UserRepository.java
@@ -2,7 +2,11 @@ package com.pharmhands.repositories;
 
 import com.pharmhands.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
  }
 
