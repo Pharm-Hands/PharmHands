debac5e7ac2c4329217d51f027be838d68a10acd
jaya <jayaseyyadri@gmail.com>
2021-03-13 08:36:46 -0600

    patient profile view , trying to add logic in controller

--- src/main/java/com/pharmhands/controllers/PhViewController.java
+++ src/main/java/com/pharmhands/controllers/PhViewController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class phViewController {
+public class PhViewController {
     @GetMapping("/profile/phViewProfile")
     public String phView(){
         return "views/phViewProfile";
