6f945361ff54c4a7e1a18cf60298ef7ab6d8abf0
jaya <jayaseyyadri@gmail.com>
2021-03-13 08:38:30 -0600

    pending logic in patientProfile controller

--- src/main/java/com/pharmhands/controllers/PatientProfileController.java
+++ src/main/java/com/pharmhands/controllers/PatientProfileController.java
@@ -1,21 +1,21 @@
-package com.pharmhands.controllers;
-
-import com.pharmhands.models.User;
-import com.pharmhands.services.UserService;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class PatientProfileController {
-    @GetMapping("/profile/Patientview")
-    public String patientProfileLoad(Model model){
-
+//package com.pharmhands.controllers;
+//
+//import com.pharmhands.models.User;
+//import com.pharmhands.services.UserService;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class PatientProfileController {
+//    @GetMapping("/profile/Patientview")
+//    public String patientProfileLoad(Model model){
+//
 //        User currentUser= userService.loggedInUser();
 //
 //        model.addAttribute("patient_info",patient_info);
 //        return "views/patientProfile";
-    }
-
-
-}
+//    }
+//
+//
+//}
