8b2d81dc595505b6af27581d52aa5785ffde54b2
James Gemes <james.gemes@gmail.com>
2021-03-16 15:44:40 -0500

    working on edit function for pharmacist information

--- src/main/java/com/pharmhands/controllers/PharmacistViewController.java
+++ src/main/java/com/pharmhands/controllers/PharmacistViewController.java
@@ -1,10 +1,13 @@
 package com.pharmhands.controllers;
 
+import com.pharmhands.models.User;
 import com.pharmhands.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PharmacistViewController {
@@ -19,6 +22,19 @@ public class PharmacistViewController {
     public String pharmacistProfileView( Model model,@PathVariable long id) {
         model.addAttribute("user", userDao.getOne(id));
         return "views/pharmacist/pharmacistProfile";
+    }
+
+    @GetMapping("pharmacistProfile/{id}edit")
+    public String editPharmacistInfoForm(Model model, @PathVariable long id){
+        User user = userDao.getOne(id);
+        model.addAttribute("user", user);
+        return "/views/pharmacist/pharmacistInfoEdit";
+    }
+
+    @PostMapping(path = "pharmacistProfile/{id}edit")
+    public String editPharmacistInfo(@ModelAttribute User user){
+        userDao.save(user);
+        return "redirect:/pharmacistProfile/{id}/";
     }
 
 }
