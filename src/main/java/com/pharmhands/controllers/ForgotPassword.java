e0134582463d785ba65686caaa9fede28eba4885
jaya <jayaseyyadri@gmail.com>
2021-03-23 22:16:19 -0500

    forgot password function

--- src/main/java/com/pharmhands/controllers/ForgotPassword.java
+++ src/main/java/com/pharmhands/controllers/ForgotPassword.java
@@ -3,13 +3,11 @@ package com.pharmhands.controllers;
 import com.pharmhands.models.User;
 import com.pharmhands.repositories.*;
 import com.pharmhands.services.EmailService;
-import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ForgotPassword {
@@ -20,7 +18,7 @@ public class ForgotPassword {
     private final PatientInfoRepository patientInfoDao;
     private final EmailService emailService;
 
-    public ForgotPassword(UserRepository userDao, UserRolesRepository userRolesDao, PrescriberInfoRepository prescriberInfoDao, PrescriptionsRepository prescriptionsDao, PatientInfoRepository patientInfoDao,EmailService emailService) {
+    public ForgotPassword(UserRepository userDao, UserRolesRepository userRolesDao, PrescriberInfoRepository prescriberInfoDao, PrescriptionsRepository prescriptionsDao, PatientInfoRepository patientInfoDao, EmailService emailService) {
         this.userDao = userDao;
         this.userRolesDao = userRolesDao;
         this.prescriberInfoDao = prescriberInfoDao;
@@ -29,21 +27,27 @@ public class ForgotPassword {
         this.emailService = emailService;
     }
 
-//
-//    @GetMapping("/forgotPassword")
-//    public String forgotPasswordForm(Model model){
-//        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        model.addAttribute("user",user);
-//    return "views/forgotPassword";
-//    }
-//
-//
-//    @PostMapping("/forgotPassword")
-//    public String password( User user) {
-//        User userExist = userDao.findByEmailExists(user.getEmail());
-//        emailService.prepareAndSend(userExist," Reset password","You clicked the reset password");
-//            return "views/forgotPassword";
-//        }
-
+    //
+    @GetMapping("/forgotPassword")
+    public String forgotPassword(Model model) {
+        model.addAttribute(new User());
+        return "views/forgotPassword";
     }
+
+    //
+    @PostMapping("/forgotPassword")
+    public String password(@RequestParam(name = "email") String email, User user) {
+
+        if (!user.getEmail().equals(email)) {
+            return "redirect:forgotPassword?email";
+        }
+        User userObj = userDao.findByEmail(user.getEmail());
+
+        if (userObj != null) {
+            emailService.prepareAndSend(userObj, " Reset password", "You clicked the reset password");
+        }
+        return "views/forgotPassword";
+    }
+
+}
 
--- src/main/java/com/pharmhands/repositories/UserRepository.java
+++ src/main/java/com/pharmhands/repositories/UserRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
-
+    User findByEmail(String email);
     @Query("FROM User WHERE role = ?1 AND id = ?2")
     User getOneByRole(UserRoles role, long id);
     @Query("FROM User where fullName = ?1 and phone_number = ?2")
