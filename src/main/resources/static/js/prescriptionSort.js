d008919e6e93b4717b032bffb2d0a058efe245e7
Rod-S <rodsanati@gmail.com>
2021-03-25 23:24:55 -0500

    complete sort functionality for other headings, refinement needed

--- src/main/resources/static/js/prescriptionSort.js
+++ src/main/resources/static/js/prescriptionSort.js
@@ -1,5 +1,5 @@
 //NAME HEADING asc-desc toggle
-('body').on('click', '.heading-rx-drug-name', function() {
+$('body').on('click', '.heading-rx-drug-name', function() {
     var sortArr=[];
     var prescriptions = $(this).parent().parent().parent().children().children();
     if ($('.heading-rx-drug-name').hasClass('toggle-on')) {
@@ -19,4 +19,65 @@
     sortArr.forEach((e)=> $('thead').next().append(e));
 });
 
-//
+//CREATED AT HEADING asc-desc toggle
+$('body').on('click', '.heading-rx-created', function() {
+    var sortArr=[];
+    var prescriptions = $(this).parent().parent().parent().children().children();
+    if ($('.heading-rx-created').hasClass('toggle-on')) {
+        $(prescriptions).each((index, value) => {
+            if (index !== 0) sortArr.push(value);
+        });
+        sortArr.sort((a, b) => b.children[0].innerText.localeCompare(a.children[0].innerText));
+        sortArr.forEach((e)=> $('thead').next().append(e));
+        $('.heading-rx-created').removeClass('toggle-on');
+        return;
+    }
+    $('.heading-rx-created').addClass('toggle-on');
+    $(prescriptions).each((index, value) => {
+        if (index !== 0) sortArr.push(value);
+    });
+    sortArr.sort((a, b) => a.children[0].innerText.localeCompare(b.children[0].innerText));
+    sortArr.forEach((e)=> $('thead').next().append(e));
+});
+
+//VERIFIED HEADING asc-desc toggle
+$('body').on('click', '.heading-rx-verified', function() {
+    var sortArr=[];
+    var prescriptions = $(this).parent().parent().parent().children().children();
+    if ($('.heading-rx-verified').hasClass('toggle-on')) {
+        $(prescriptions).each((index, value) => {
+            if (index !== 0) sortArr.push(value);
+        });
+        sortArr.sort((a, b) => b.children[0].innerText.localeCompare(a.children[0].innerText));
+        sortArr.forEach((e)=> $('thead').next().append(e));
+        $('.heading-rx-verified').removeClass('toggle-on');
+        return;
+    }
+    $('.heading-rx-verified').addClass('toggle-on');
+    $(prescriptions).each((index, value) => {
+        if (index !== 0) sortArr.push(value);
+    });
+    sortArr.sort((a, b) => a.children[0].innerText.localeCompare(b.children[0].innerText));
+    sortArr.forEach((e)=> $('thead').next().append(e));
+});
+
+//PATIENT HEADING asc-desc toggle
+$('body').on('click', '.heading-rx-patient-name', function() {
+    var sortArr=[];
+    var prescriptions = $(this).parent().parent().parent().children().children();
+    if ($('.heading-rx-patient-name').hasClass('toggle-on')) {
+        $(prescriptions).each((index, value) => {
+            if (index !== 0) sortArr.push(value);
+        });
+        sortArr.sort((a, b) => b.children[0].innerText.localeCompare(a.children[0].innerText));
+        sortArr.forEach((e)=> $('thead').next().append(e));
+        $('.heading-rx-patient-name').removeClass('toggle-on');
+        return;
+    }
+    $('.heading-rx-patient-name').addClass('toggle-on');
+    $(prescriptions).each((index, value) => {
+        if (index !== 0) sortArr.push(value);
+    });
+    sortArr.sort((a, b) => a.children[0].innerText.localeCompare(b.children[0].innerText));
+    sortArr.forEach((e)=> $('thead').next().append(e));
+});
